4c023e10e1845695f197fa4cbae903a8601b60fc
Administrator <Administrator@User-2022CMXZHE>
2022-12-02 11:11:55 +0900

    537

---
+++ src/q537/Main1.java
@@ -0,0 +1,19 @@
+package q537;
+
+import java.util.Scanner;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int inp = sc.nextInt();
+		sc.close();
+
+		int sum = 0, i = 1;
+		while (i <= inp) {
+			sum += i++;}
+			System.out.println(sum);
+			
+		
+	}
+}
