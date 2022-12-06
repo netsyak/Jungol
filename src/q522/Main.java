54b4ff29c02eddbdaf070446b09e9d87860a2c78
Administrator <Administrator@User-2022CMXZHE>
2022-12-06 12:27:57 +0900

    연산자

---
+++ src/q522/Main.java
@@ -0,0 +1,15 @@
+package q522;
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.close();
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		boolean a = (x!=0);
+	}
+
+}
