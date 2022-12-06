1f550752fd9fd879ac383252ecc5dc859271e9fa
Administrator <Administrator@User-2022CMXZHE>
2022-12-06 17:26:21 +0900

    선택제어문

---
+++ src/q9030/Main.java
@@ -0,0 +1,17 @@
+package q9030;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		 System.out.println(num);
+		if (num > 10) {
+			System.out.println("10보다 큰 수를 입력하셨습니다.");
+		}
+		sc.close();
+	}
+
+}
