e54fa929d1f2f81dae1464fe9b7934626e672170
Administrator <Administrator@User-2022CMXZHE>
2022-12-06 17:23:11 +0900

    연산자

---
+++ src/q9026/Main.java
@@ -0,0 +1,21 @@
+package q9026;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a = 0;
+		int b = 1;
+		int c = 2;
+
+		int add = a + b;
+		int minus = c-b ;
+		int multi = a*b;
+		int div = b%c;
+		
+		
+		System.out.print(add+" "+ minus+" "+multi+" "+div);
+	
+	}
+
+}
