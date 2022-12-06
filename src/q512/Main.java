19d25e0ba41a70ce5fa39e70e6a007e02dd5f7f7
Administrator <Administrator@User-2022CMXZHE>
2022-12-06 11:47:26 +0900

    q512

---
+++ src/q512/Main.java
@@ -0,0 +1,18 @@
+package q512;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		double a=49;
+//		double b=0.2683;
+//		
+//		System.out.printf(a + " * " + b + " = "+ "%.6f",a*b);
+//		
+		int weight= 49;
+		double ratio = 0.2683;
+		
+		//System.out.printf(weight + " * " + ratio + " = "+ (weight*ratio)); 
+		System.out.printf("%d * %f = %f\n", weight, ratio, weight * ratio);
+	}
+
+}
