2a07dfb706bf261443356f65a31cfeefaff436ef
Administrator <Administrator@User-2022CMXZHE>
2022-12-06 11:46:48 +0900

    q506

--- src/q506/Main.java
+++ src/q506/Main.java
@@ -3,24 +3,17 @@ package q506;
 public class Main {
 
 	public static void main(String[] args) {
-//		int a = 170;
-//		double b = 68.6;
-//				
-//		System.out.println("My height");
-//		System.out.println(a);
-//		System.out.println("My weight");
-//		System.out.printf("%.6f", b);
-//		
-	String str1 = "My height";
-	int height = 170;
-	String str2 = "My weight";
-	double weight = 68.6;
-	
-	System.out.println(str1);
-	System.out.println(height);
-	System.out.println(str2);
-	System.out.printf("%.6f", weight);
-	
+		System.out.println();
+		String str1 = "My height";
+		int height = 170;
+		String str2 = "My weight";
+		double weight = 68.6;
+
+		System.out.printf(str1);
+		System.out.println(height);
+		System.out.println(str2);
+		System.out.printf("%.6f", weight);
+
 	}
 
 }
