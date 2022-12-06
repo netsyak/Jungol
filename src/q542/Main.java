d4c13b1421e8d79303f1938dd4ba4a31c7ee5f94
Administrator <Administrator@User-2022CMXZHE>
2022-12-06 13:11:35 +0900

    반복제어문2

---
+++ src/q542/Main.java
@@ -0,0 +1,26 @@
+package q542;
+
+public class Main {
+	public static void main(String[] args) {
+
+//		//방법 1
+//		for (int i = 10; i <= 20; i++) {
+//			System.out.print(i + " ");
+
+//		방법 2
+//		int i = 10;
+//		while (i <= 20) {
+//			System.out.print(i + " ");
+//			i++;
+		
+//		방법 3
+		int i=10;
+		do {
+			System.out.print(i+" ");
+			i++;
+		}
+		while(i<=20);
+	
+
+	}
+}
---
+++ src/q542/Main1.java
@@ -0,0 +1,25 @@
+package q542;
+
+public class Main1 {
+	public static void main(String[] args) {
+//	for 문
+		for (int i =10; i<21; i++) {
+			System.out.print(i +" ");
+		}
+		System.out.println();
+//	while 문
+		int j = 10;
+		while (j < 21) {
+			System.out.print(j + " ");
+			j++;
+		}
+		System.out.println();
+//	do-while 문
+		int k = 10;
+		do {
+			System.out.print(k + " ");
+			k++;
+		} while (k < 21);
+		System.out.println();
+	}
+}
