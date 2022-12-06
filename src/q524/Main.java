d176e818835769deca27fb9afb8ec84134a5d7fd
Administrator <Administrator@User-2022CMXZHE>
2022-12-06 12:28:25 +0900

    연산자

---
+++ src/q524/Main.java
@@ -0,0 +1,20 @@
+package q524;
+
+import java.util.Scanner ;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+	
+		boolean c = (a!= 0);
+		boolean d = (b!= 0);
+			
+		System.out.print((c && d)+" "+ (c || d));
+					
+	}
+      
+}
