a1d7795a9e77d294f6ee42b4c006f88c6a653f58
Administrator <Administrator@User-2022CMXZHE>
2022-12-06 11:47:16 +0900

    q511

---
+++ src/q511/Main.java
@@ -0,0 +1,25 @@
+package q511;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//		int x = 55; 
+		//		int y = 10;
+		//		int z = x-y;
+		//		
+		//		int num1 = 2008;
+		//		int num2 = 1999;
+		//		int num3 = num1-num2;
+		//				
+		//		System.out.println("55 - 10 = "+ z);
+		//		System.out.println("2008 - 1999 = "+ num3);
+		int x= 55, y= 10; 
+		System.out.printf("%d - %d = %d\n",x,y,(x-y));
+
+		int a= 2008, b= 1999;
+		System.out.printf("%d - %d = %d",a,b,(a-b));
+
+
+	}
+
+}
