3f658e30f15b18ca06467c5712f3f07cc9cc3c79
Administrator <Administrator@User-2022CMXZHE>
2022-12-07 10:26:42 +0900

    .

--- src/q9041/Main.java
+++ src/q9041/Main.java
@@ -6,19 +6,32 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+// 1.문제
+//		do {
+//			System.out.print("점수를 입력하세요. ");
+//			int score = sc.nextInt();
+//			if (score >= 80 && score <= 100) {
+//				System.out.println("축하합니다. 합격입니다.");
+//			} else if (score < 80 && score > 0) {
+//				System.out.println("죄송합니다. 불합격입니다.");
+//			} else {
+//				sc.close();
+//				break;
+//			}
+//
+//		} while (true);
 
-		do {
+		for (;;) {
 			System.out.print("점수를 입력하세요. ");
-			int score = sc.nextInt();
-			if (score >= 80 && score <= 100) {
+			int inp = sc.nextInt();
+			if (inp >= 80 && inp <= 100) {
 				System.out.println("축하합니다. 합격입니다.");
-			} else if (score < 80 && score > 0) {
+			} else if (inp < 80 && inp > 0) {
 				System.out.println("죄송합니다. 불합격입니다.");
 			} else {
 				sc.close();
 				break;
 			}
-
-		} while (true);
+		}
 	}
 }
