f882d74039a95dc6624ec6af0fc43a7ddaa7a10f
Administrator <Administrator@User-2022CMXZHE>
2022-12-07 09:14:19 +0900

    .

---
+++ src/q9020X/Main.java
@@ -0,0 +1,22 @@
+package q9020;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int [] num = new int[5];
+		System.out.print(num[i]+" ");
+		
+		for( int i = 0 ; i<num.length ; i++ ) {
+			num[i] = sc.nextInt();
+		
+			System.out.print(num[i]+" ");
+		
+			
+		}
+		sc.close();
+	}
+
+}
